e03792b2ac1dcdf1fe7a9a0e245f527135a91463
iwls <ian.willems@gmail.com>
2015-04-26 22:05:48 +0200

    finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# makeCacheMatrix is function containing other functions
+#in the function you can find short comments
 makeCacheMatrix <- function(x = matrix()) {
   # empty value of reversed matrix
   m <- NULL
@@ -21,13 +22,15 @@ makeCacheMatrix <- function(x = matrix()) {
   GetReverse <- function() {
     m
   }
-  
+  # create a list containing all the functions in the function MakeCacheMatrix
   list (Set= Set, Get= Get , SetReverse = SetReverse, GetReverse=GetReverse)
 }
 
 
 ## Write a short comment describing this function
-
+# CacheSolve is function checking if the reverse is already in cache or not, 
+#if not i will calculate the reverse and return it, else it will return it from the Cache
+#in the function you can find short comments
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$GetReverse()
